0370d454b9c9dea8832fdcf35a6d3b7b3dbba832
hhllhhyyds <hhllhhyyds@sina.com>
2024-04-17 18:10:36 +0800

    Create SingleLinkedList struct

--- Basic/data_structures/src/linked_list.rs
+++ Basic/data_structures/src/linked_list.rs
@@ -1,1 +1,23 @@
+#[derive(Debug)]
+pub struct SingleLinkedNode<T> {
+    value: T,
+    next: SingleLinkedList<T>,
+}
 
+#[derive(Debug)]
+pub struct SingleLinkedList<T>(Option<Box<SingleLinkedNode<T>>>);
+
+impl<T: PartialEq> SingleLinkedList<T> {
+    pub fn search(&self, x: &T) -> Option<&SingleLinkedNode<T>> {
+        match &self.0 {
+            Some(node) => {
+                if node.value.eq(x) {
+                    return Some(node);
+                } else {
+                    node.next.search(x)
+                }
+            }
+            None => None,
+        }
+    }
+}
