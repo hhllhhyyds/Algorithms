e52471c790b53fb57775d862fb895e8e1c42659d
hhllhhyyds <hhllhhyyds@sina.com>
2024-04-29 13:44:50 +0800

    Impl Queue for BinaryTree

--- Basic/data_structures/src/queue.rs
+++ Basic/data_structures/src/queue.rs
@@ -1,4 +1,4 @@
-use crate::linked_list::SingleLinkedList;
+use crate::{linked_list::SingleLinkedList, tree::BinaryTree};
 
 pub trait Queue<T> {
     fn enqueue(&mut self, x: T);
@@ -47,8 +47,52 @@ impl<T> Queue<T> for SingleLinkedList<T> {
     }
 }
 
+#[derive(Debug)]
+struct Node<T>(usize, T);
+impl<T> PartialEq for Node<T> {
+    fn eq(&self, other: &Self) -> bool {
+        self.0.eq(&other.0)
+    }
+}
+impl<T> PartialOrd for Node<T> {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        self.0.partial_cmp(&other.0)
+    }
+}
+
+impl<T: std::fmt::Debug> Queue<T> for BinaryTree<Node<T>> {
+    fn enqueue(&mut self, x: T) {
+        let index = self
+            .max()
+            .map(|node| node.borrow().value.0 + 1)
+            .unwrap_or(0);
+        self.insert(Node(index, x))
+    }
+
+    fn dequeue(&mut self) -> Option<T> {
+        let min = self.min()?;
+        self.remove(min.clone());
+        Some(std::rc::Rc::try_unwrap(min).unwrap().into_inner().value.1)
+    }
+
+    fn size(&self) -> usize {
+        if self.is_empty() {
+            return 0;
+        }
+        let max_index = self.max().unwrap().borrow().value.0;
+        let min_index = self.min().unwrap().borrow().value.0;
+        max_index - min_index + 1
+    }
+
+    fn new() -> Self {
+        BinaryTree::new()
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use rand::Rng;
+
     use super::*;
 
     #[test]
@@ -78,6 +122,26 @@ mod tests {
 
         for i in 0..10 {
             assert!(i == queue.dequeue().unwrap());
+        }
+
+        assert!(queue.size() == 0);
+    }
+
+    #[test]
+    fn test_tree_queue() {
+        let mut queue: BinaryTree<_> = Queue::new();
+        let mut rng = rand::thread_rng();
+        let input = (0..100)
+            .map(|_| rng.gen_range(0..10000))
+            .collect::<Vec<_>>();
+        for x in &input {
+            queue.enqueue(*x);
+        }
+
+        assert!(queue.size() == 100);
+
+        for i in 0..100 {
+            assert!(input[i] == queue.dequeue().unwrap());
         }
 
         assert!(queue.size() == 0);
